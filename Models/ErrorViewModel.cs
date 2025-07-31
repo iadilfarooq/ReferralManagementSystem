eea93241b86d2db219fa1484033162d2fbc03b90
Adil Farooq <adil.f1@outlook.com>
2025-07-31 23:26:51 +0500

    Update ErrorViewModel.cs
    add error handling

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace ReferralManagementSystem.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+public string ErrorName{get; set;}
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
